e20969b8fb0c02c83d4e9073f71f757a85246899
bala1802 <bala1802@live.com>
2024-01-05 20:09:02 +0530

    fixed download return statement

--- UNET/download.py
+++ UNET/download.py
@@ -1,5 +1,5 @@
 import torchvision
 
 def download_data(path, split):
-    pet_dataset = torchvision.datasets.OxfordIIITPet(root=path, split=split, target_types="segmentation", download=True)
+    return torchvision.datasets.OxfordIIITPet(root=path, split=split, target_types="segmentation", download=True)
     
