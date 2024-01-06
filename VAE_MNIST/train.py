cfbdb4ae9b46e997a161a7d9923a1c9dc05ac35b
bala1802 <bala1802@live.com>
2024-01-06 07:33:03 +0530

    implemented training script

--- VAE_MNIST/train.py
+++ VAE_MNIST/train.py
@@ -4,9 +4,9 @@ from pytorch_lightning.callbacks import LearningRateMonitor
 
 from vae_model import VAE
 
-def train(train_dataloader ,val_dataloader):
+def train(train_dataloader, val_dataloader):
     pl.seed_everything(1234)
     model = VAE().to('cuda')
     trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=20, logger= [CSVLogger(save_dir="logs/")],
                     callbacks=[LearningRateMonitor(logging_interval="step")])
-    trainer.fit(model, train_dataloader ,val_dataloader)
+    trainer.fit(model, train_dataloader, val_dataloader)
