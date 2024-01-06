1006472d040a5aa41824f46d59d84073b4d7c25a
bala1802 <bala1802@live.com>
2024-01-06 12:21:32 +0530

    updated the steps per epoch to 2084

--- VAE_CIFAR10/config.py
+++ VAE_CIFAR10/config.py
@@ -3,5 +3,5 @@ import torch
 MAX_EPOCHS=40
 LEARNING_RATE = 1e-4
 MAX_LEARNING_RATE = 1E-3
-STEPS_PER_EPOCH = 1875 #FIXME
+STEPS_PER_EPOCH = 2084
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
